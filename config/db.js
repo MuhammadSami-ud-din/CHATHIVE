0565088485029f139e68c4bdfabc264274c27e19
Muhammad Sami-Ud-Din <muhammadsmmdn2007@gmail.com>
2026-07-30 01:39:18 +0500

    feat: MySQL connection pool setup

--- config/db.js
+++ config/db.js
@@ -6,7 +6,7 @@ const pool = mysql.createPool({
     host : process.env.DB_HOST,
     port : process.env.DB_PORT,
     password : process.env.DB_PASSWORD,
-   
+    database: process.env.DB_NAME,
     user : process.env.DB_USER
 
 })
