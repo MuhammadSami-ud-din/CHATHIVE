1a08cd9a31c4b07e8d1e54164f2425fd302c6ab4
Muhammad Sami-Ud-Din <muhammadsmmdn2007@gmail.com>
2026-08-02 22:17:40 +0500

    setting up and testing socket.io

--- server.js
+++ server.js
@@ -2,14 +2,26 @@
 require('dotenv').config();
 
 const express = require ('express');
+const http = require('http');
+const {Server} = require('socket.io')
 const app = express();
+
+const server = http.createServer(app);
+const io = new Server(server , {
+    cors : {
+        origin : '*'
+    }
+}
+)
+
 const cors = require('cors');
 require('./config/mongo.js');
 require ("./models/message.js")
 const pool = require('./config/db.js');
-
 const port = process.env.PORT || 3000
 const verifyToken = require('./middleware/authMiddleWare.js');
+
+
 
 const loginRoute = require('./routes/login.js');
 const registerRoute = require('./routes/register.js');
@@ -18,11 +30,10 @@ const channelRoute = require('./routes/channels.js');
 const joinRoute = require('./routes/join.js');
 const messageRoute = require('./routes/messages.js');
 const DMRoute = require('./routes/directMessages.js');
+const { Socket } = require('dgram');
 
 app.use(express.json());
 app.use(cors());
-
-
 
 
 app.use(loginRoute);
@@ -45,6 +56,15 @@ app.get('/test-db' ,verifyToken, async(req , res)=>{
     
 })
 
+io.on('connection' , (socket)=>{
+   console.log(' A user Connected' , socket.id);
+
+   socket.on('disconnect' , ()=>{
+    console.log('A user disconnected' , socket.id);
+   })
+
+})
+
 // app.get('/' , (req , res) =>{
 //     const responseData = {
 //       message : "AOA bhai jaan aap ka backend kaam kr rha hai chawlien marna bnd kro",
@@ -54,6 +74,6 @@ app.get('/test-db' ,verifyToken, async(req , res)=>{
 //     res.json(responseData);
 // })
 
-app.listen(port , ()=>{
+server.listen(port , ()=>{
     console.log(`hello u are a fool and servers is running on port ${port}`)
 })
