6a44bebcae9839d0ed2e202c076c4bdf7e567e29
Muhammad Sami-Ud-Din <muhammadsmmdn2007@gmail.com>
2026-08-27 00:19:47 +0500

    server_img route

--- routes/user.js
+++ routes/user.js
@@ -28,6 +28,52 @@ router.post('/avatar-upload/:server_id', verifyToken, (req, res) => {
             const imageUrl = req.file.path;
 
             const [updateResult] = await pool.query(
+                `UPDATE servers SET avatar = ? WHERE server_id = ?`,
+                [imageUrl , server_id]
+            );
+
+            if (updateResult.affectedRows === 0) {
+                return res.status(404).json({ error: "User not found" });
+            }
+
+            await redis.del('channels:all-' + server_id)
+
+
+            return res.status(200).json({
+                message: "Profile picture updated successfully",
+                avatar : imageUrl
+            });
+
+        } catch (dbError) {
+            console.error("Database Update Error:", dbError);
+            return res.status(500).json({ error: "Failed to update database profile" });
+        }
+    });
+});
+
+router.post('/img-upload/:server_id', verifyToken, (req, res) => {
+    const {server_id} = req.params
+  
+    upload.single('Image')(req, res, async (err) => {
+        if (err) {
+            // THIS IS WHERE YOUR OBJECT ERROR WAS HIDDEN:
+            console.error("Multer / Cloudinary Upload Error Details:", err);
+            return res.status(500).json({
+                error: err.message || "Cloudinary upload failed",
+                details: err
+            });
+        }
+
+        try {
+            const userId = req.user.id;
+
+            if (!req.file) {
+                return res.status(400).json({ error: 'Please upload an image file.' });
+            }
+
+            const imageUrl = req.file.path;
+
+            const [updateResult] = await pool.query(
                 `UPDATE servers SET server_img = ? WHERE server_id = ?`,
                 [imageUrl , server_id]
             );
